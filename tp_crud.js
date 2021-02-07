b2744e171ad295103ea9d5a69c95ef916706f366
Simon Rossini <simonrossini09.sr@gmail.com>
2021-02-07 11:58:46 +0100

    tp-crud05022021

---
+++ tp_crud.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+
+	$('.block').();
+
+	$('#submit').click();
+
+	var tel = $('#tel');
+
+	tel.bind('keyup', function () {
+
+		var mobile = /^(01|02|03|04|05|06|07|08|09)[0-9]{8}$/;
+	
+	if (!mobile.test($('#tel').val())) {
+		alert('Numero KO')
+		$('#tel').focus();
+		
+		return false;}
+		
+		}
+	})
+
+
+		//$(document).ready(function () {
+			
+			    $ function (split form){
+				  var prenom;
+				  var nom;
+				  var adresse;
+				  var tel;
+				
+				
+				JQuery(#maListe > tbody:last).append(<tr>id<\/tr> <tr>nom<\/tr> <tr>prenom<\/tr> <tr>adresse<\/tr> <tr>tel<\/tr>);
+				{for (id=0; id <= #maListe.length; id= ++) }
+
+				$ form(nom.getElementsByTagName(name='""')) ;
+				this.value = value
+				{
+				  value = value.split('').getElementsByTagName('')
+				$ class RegExp1 extends RegExp {[Symbol.replace](str) 
+					{return RegExp.prototype[Symbol.replace].call(value, str, name)
+					  }
+					}
+				} 
+				
+				$ form(prenom.getElementsByTagName(name='""')) 
+				this.value = value
+				{
+				  value = this.split('').getElementsByTagName('')
+				}
+				$ class RegExp1 extends RegExp {[Symbol.replace](str) 
+					{return RegExp.prototype[Symbol.replace].call(value, str, name)
+					  }
+					}
+				} 
+				  
+								
+				$ form(email).push(encodeURIComponent(adresse) + '=' + encodeURIComponent(data[adresse]));
+
+				$ form.'adresse'=value
+				{
+				  value = value.split('').getElementsByTagName('')
+			      class RegExp1 extends RegExp {[Symbol.replace](str) 
+					{return RegExp.prototype[Symbol.replace].call(value, str, name)
+					  }
+					}
+				 				  
+				$ form(tel) {
+				    
+				  tel.value = value	
+
+				$ value.split('').getElementsByTagName('')
+				$ class RegExp1 extends RegExp {[Symbol.replace](str) 
+					{return RegExp.prototype[Symbol.replace].call(value, str, name)
+					  }
+					
+		//		 addEventListener('ajout', function(event) {
+		//  alert('Ouais ! Données envoyées');
+		 /*};
+				addEventListener('error', function(event) {
+		    alert('Oups! Quelque chose s\'est mal passé.');
+		  };
+ 
+	
+	
+	  }		
+
+	
